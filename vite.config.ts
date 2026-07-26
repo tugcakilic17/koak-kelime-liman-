3920d435da5e8809f64fad11ed00f054da31716a
tugca <tugcakilic@gmail.com>
2026-07-26 03:06:32 +0300

    GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/koak-kelime-liman-/',
   server: {
     host: '127.0.0.1',
     port: 5173,
